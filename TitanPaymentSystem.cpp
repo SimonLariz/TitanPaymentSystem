bd5f279f0e0ade1b775a6d99253dc21f65eecc09
SimonLariz <50644041+SimonLariz@users.noreply.github.com>
2022-10-24 15:20:38 -0700

    Updated main

--- TitanPaymentSystem.cpp
+++ TitanPaymentSystem.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
 #include "Purchase.h"
+#include "User.h"
 
 using namespace std;
 
 int main()
 {
     cout << "Hello, Welcome to the Titan Payment System\n";
-    Purchase temp;
-    temp.setPurchaseDate(221009);
-    temp.setPurchaseCard("TEST");
-    temp.setAmountPaid(111.02);
-    cout << temp.getPurchaseDate() << '\n';
-    cout << temp.getPurchaseCard() << '\n';
-    cout << temp.getAmountPaid() << '\n';
+    //Testing Purchase Class
+    cout << "Testing Purchase Class\n";
+    Purchase tempPurchase;
+    tempPurchase.setPurchaseDate(221009);
+    tempPurchase.setPurchaseCard("TEST");
+    tempPurchase.setAmountPaid(111.02);
+    cout << tempPurchase.getPurchaseDate() << '\n';
+    cout << tempPurchase.getPurchaseCard() << '\n';
+    cout << tempPurchase.getAmountPaid() << "\n\n\n";
 
+    //Testing User Class
+    cout << "Testing User Class\n";
+    User tempUser;
+    tempUser.setFirstName("Bob");
+    tempUser.setLastName("Mill");
+    tempUser.setUserCountry("United States");
+    cout << tempUser.getFirstName() << '\n';
+    cout << tempUser.getLastName() << '\n';
+    cout << tempUser.getCountry() << "\n\n\n";
 
     return 0;
 }
