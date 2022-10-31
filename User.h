a17bbc11834fe574b55824ba3af5a500ea11252b
SimonLariz <50644041+SimonLariz@users.noreply.github.com>
2022-10-31 15:00:49 -0700

    Update User.h

--- User.h
+++ User.h
@@ -6,33 +6,41 @@
 class User {
 private:
 	// Variables
+	std::string userName;
 	std::string userFirstName;
 	std::string userLastName;
-	std::string userFullName;
 	std::string userPhoneNumber;
 	std::string userPassword;
 	std::string userCountry;
 	std::string userAddress;
+
 	std::vector<Purchase> purchaseVect;
+	//NEW
+	double totalAmountPaid;
+	double amountDue;
 
 public:
 	//Adds purchase to purchase vector
-	void addPurchase(std::string date, std::string card, std::string amount, std::string status);
+	void addPurchase(std::string date, std::string card, std::string amount);
 
 	//Setter Functions
 	void setFirstName(std::string firstName);
 	void setLastName(std::string lastName);
-	void setUserFullName(std::string fullName);
+	void setUserName(std::string fullName);
 	void setUserPhoneNumber(std::string phoneNumber);
 	void setUserPassword(std::string password);
 	void setUserCountry(std::string country);
 	void setUserAddress(std::string address);
+	void setAmountDue(double amount);
+	void setTotalAmountPaid(double amount);
 	//Getter Functions
 	std::string getFirstName();
 	std::string getLastName();
-	std::string getFullName();
+	std::string getUserName();
 	std::string getPhoneNumber();
 	std::string getUserPassword();
 	std::string getCountry();
 	std::string getAddress();
+	double getAmountDue();
+	double getTotalAmountPaid();
 };
